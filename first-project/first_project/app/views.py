c603e760add4cf43a4f10757bdddb5a4fece1279
Lyubov Martemyanova <martemyanova.liubov@gmail.com>
2023-08-10 22:06:50 +0300

    workdir html

--- first-project/first_project/app/views.py
+++ first-project/first_project/app/views.py
@@ -7,16 +7,11 @@ from django.shortcuts import render, reverse
 
 def home_view(request):
     template_name = 'app/home.html'
-    # впишите правильные адреса страниц, используя
-    # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
         'Показать текущее время': reverse('time'),
         'Показать содержимое рабочей директории': reverse('workdir')
     }
-
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
     context = {
         'pages': pages
     }
@@ -24,19 +19,15 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
     current_time = datetime.datetime.now().time()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
     try:
-        listdir = ", ".join(os.listdir())
-        return HttpResponse(listdir)
+        template_name = 'app/workdir.html'
+        listdir = {'listdir': os.listdir()}
+        return render(request, template_name, listdir)
     except Exception:
         raise NotImplemented
