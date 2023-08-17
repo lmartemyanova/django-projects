ed50bad3fecb70f32a73933122370b327c099e7a
Lyubov Martemyanova <martemyanova.liubov@gmail.com>
2023-08-17 19:05:48 +0300

    recipes calculator

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,16 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def recipe(request, dish):
+    servings = int(request.GET.get('servings', 1))
+    rec = {}
+    if dish in DATA.keys():
+        for ingredient, quantity in DATA[dish].items():
+            rec[ingredient] = round(quantity * servings, 2)
+    context = {
+        'recipe': rec
+    }
+    return render(request, 'calculator/index.html', context)
