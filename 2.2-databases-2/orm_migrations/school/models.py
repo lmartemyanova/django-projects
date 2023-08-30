ac2987d63479bca79cae90a1763094f336207a6a
Lyubov Martemyanova <martemyanova.liubov@gmail.com>
2023-08-30 21:43:31 +0300

    clearing comments and ordering

--- 2.2-databases-2/orm_migrations/school/models.py
+++ 2.2-databases-2/orm_migrations/school/models.py
@@ -15,12 +15,13 @@ class Teacher(models.Model):
 
 class Student(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя')
-    teachers = models.ManyToManyField(Teacher, related_name='students', verbose_name='Учитель')
+    teachers = models.ManyToManyField(Teacher, related_name='students', verbose_name='Учителя')
     group = models.CharField(max_length=10, verbose_name='Класс')
 
     class Meta:
         verbose_name = 'Ученик'
         verbose_name_plural = 'Ученики'
+        ordering = ['group']
 
     def __str__(self):
         return self.name
